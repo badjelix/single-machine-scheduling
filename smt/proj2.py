0a1443b4b833be2398a2f2b5e43bd74e4ff61cbb
João Palet <joao.palet@outlook.com>
2020-11-18 15:19:05 +0000

    1st version working

--- smt/proj2.py
+++ smt/proj2.py
@@ -78,10 +78,12 @@ def solve():
                 for bj in range(1, nk[bi]+1):
                     solver.add(Or( Or(T[i]==0, T[bi]==0), If(K[i][j] > K[bi][bj], K[i][j] >= K[bi][bj] + pk[bi][bj], K[bi][bj] >= K[i][j] + pk[i][j])))
 
+
     # # Task can only start if all its dependencies are done
-    # for i in range(1, nt+1):
-    #     for dep in deps[i]:
-    #         solver.add(If(T[dep] == 1, Or(T[i] == 0,  K[i][0] >= K[dep][nk[dep]] + pk[dep][nk[dep]]), T[i] == 0))
+    for i in range(1, nt+1):
+        for dep in deps[i]:
+            solver.add(If(T[dep] == 1, Or(T[i] == 0,  K[i][1] >= K[dep][nk[dep]] + pk[dep][nk[dep]]), T[i] == 0))
+
 
     solver.maximize(Sum([t for t in T]))
 
